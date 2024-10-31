f8a8117e5a256cf924d668a84a748b1551e7cea2
mehmet.ozturk <mehmet.ozturk@avalabs.org>
2024-10-31 16:42:33 +0100

    feat: deployment script implemented

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,9 @@
 import { ethers } from "hardhat";
 import { deployLibrary, deployVerifiers } from "../test/helpers";
+import {
+	EncryptedERC__factory,
+	Registrar__factory,
+} from "../typechain-types/factories/contracts";
 
 const main = async () => {
 	const [deployer] = await ethers.getSigners();
@@ -17,6 +21,28 @@ const main = async () => {
 	const babyJubJub = await deployLibrary(deployer);
 	console.log("---- Library ----");
 	console.log(`BabyJubJub            : ${babyJubJub}`);
+
+	const registrarFactory = new Registrar__factory(deployer);
+	const registrar = await registrarFactory.deploy(registrationVerifier);
+	console.log("---- Registrar ----");
+	console.log(`Registrar             : ${registrar.target}`);
+
+	const encryptedERCFactory = new EncryptedERC__factory({
+		"contracts/libraries/BabyJubJub.sol:BabyJubJub": babyJubJub,
+	});
+	const encryptedERC = await encryptedERCFactory.connect(deployer).deploy({
+		_registrar: registrar.target,
+		_isConverter: false, // lets leave it false for now
+		_name: "Encrypted ERC",
+		_symbol: "EERC",
+		_mintVerifier: mintVerifier,
+		_burnVerifier: burnVerifier,
+		_transferVerifier: transferVerifier,
+	});
+	await encryptedERC.waitForDeployment();
+
+	console.log("---- EncryptedERC ----");
+	console.log(`EncryptedERC          : ${encryptedERC.target}`);
 };
 
 main();
